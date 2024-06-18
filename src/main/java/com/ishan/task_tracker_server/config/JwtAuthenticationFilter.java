4fd1c183fea859d64bd921b1dc5085b5aa3c5f60
kshenukaishan <kshenukaishan@outlook.com>
2024-06-18 12:01:21 +0530

    defined jwtAuthFiler class extended oncePerRequest

---
+++ src/main/java/com/ishan/task_tracker_server/config/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.ishan.task_tracker_server.config;
+
+import com.ishan.task_tracker_server.service.jwt.UserService;
+import com.ishan.task_tracker_server.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+}
