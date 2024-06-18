e18f90d392800c437a1b5f7fc137e01cb003f92b
kshenukaishan <kshenukaishan@outlook.com>
2024-06-18 15:24:24 +0530

    defined WebSecurityConfig class

---
+++ src/main/java/com/ishan/task_tracker_server/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.ishan.task_tracker_server.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+
+
+}
