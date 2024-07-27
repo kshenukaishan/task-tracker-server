a733575da0d018245e8371571e0f1852718766af
kshenukaishan <kshenukaishan@outlook.com>
2024-07-27 19:01:10 +0530

    Initialized app

---
+++ src/main/java/com/ishan/task_tracker_server/TaskTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.task_tracker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishan/task_tracker_server/TaskTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.task_tracker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
